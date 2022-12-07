59a601b38ce60bab4a6c9ff2e5331edd96869111
drknozan <drknozan@gmail.com>
2022-12-07 22:27:40 +0300

    Create products index page

---
+++ pages/products/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import Filters from "../../components/Filters";
+import Product from "../../components/Product";
+import clientPromise from "../../utils/mongodb";
+
+export default function Products({ products }) {
+
+    return (
+        <div>
+            <Head>
+
+            </Head>
+            <div className="container mx-auto">
+                <div className="flex">
+                    <div className="w-1/4">
+                        <Filters />
+                    </div>
+                    <div className="w-3/4 pb-20">
+                        <div className="grid grid-cols-3">
+                            {products.length > 0 && products.map((product, i) => {
+                                    return <Product name={product.name} imageUrl={product.imageUrl} price={product.price} productCode={product.productCode} key={i} />
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export async function getServerSideProps() {
+    try {
+        const client = await clientPromise;
+        const db = client.db("store");
+    
+        let products = await db
+          .collection("product")
+          .find({})
+          .limit(10)
+          .toArray();
+
+        products = products.map((product) => { const { _id, ...rest } = product; return rest});
+        return {
+            props: { products }, 
+        }
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
