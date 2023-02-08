5f397f8e1a9b6bccbd8d7b73736ecf8115b15f7c
drknozan <drknozan@gmail.com>
2023-02-08 16:53:59 +0300

    refactor: form submissions

--- pages/login.js
+++ pages/login.js
@@ -1,39 +1,19 @@
 import { useRouter } from "next/router";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import * as Yup from "yup";
 import { useState } from "react";
-import { signIn } from "next-auth/react";
+import { signIn } from "next-auth/react"
 
-export default function Register() {
+export default function Login() {
+    const [alert, setAlert] = useState("");
     const router = useRouter();
-    const [serverErorr, setServerErorr] = useState("");
 
-    const formik = useFormik({
-        initialValues: {
-            email : "",
-            password: "",
-        },
-        validationSchema: Yup.object({
-            email: Yup.string()
-                .email('Invalid email address.').required('You have to provide email address.'),
-            password: Yup.string()
-                .required('Please enter password.')
-        }),
-        onSubmit: async (values) => {
-            const res = await signIn("credentials", {
-                redirect: false,
-                email: values.email,
-                password: values.password,
-                callbackUrl: "/",
-            });
-
-            if (res.ok) {
-                router.push("/");
-            } else {
-                setServerErorr("Invalid username or password.");
-            }
-        },
-    });
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .email('Invalid email address.').required('You have to provide email address.'),
+        password: Yup.string()
+            .required('Please enter password.')
+    })
 
     return (
         <div>
@@ -43,14 +23,43 @@ export default function Register() {
                     <div className="my-6 text-gray-600 text-center text-2xl">
                         Login
                     </div>
-                    <form className="flex flex-col items-center" onSubmit={formik.handleSubmit}>
-                        <input id="email" name="email" type="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="E-mail"></input>
-                        { formik.errors.email && formik.touched.email ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{formik.errors.email}</div> : null }
-                        <input id="password" name="password" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="Password"></input>
-                        { formik.errors.password && formik.touched.password ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{formik.errors.password}</div> : null }
-                        { serverErorr && <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{serverErorr}</div> }
-                        <button type="submit" className="flex border mt-8 rounded px-8 py-2 text-sm mx-auto border-indigo-500 text-indigo-500">Login</button>
-                    </form>
+                    <Formik
+                        initialValues={{ email: "", password: "" }}
+                        validationSchema={validationSchema}
+                        onSubmit={async (values) => {
+                            let res = await signIn("credentials", {
+                                redirect: false,
+                                email: values.email,
+                                password: values.password,
+                                callbackUrl: "/",
+                            });
+                
+                            if (res.ok) {
+                                setAlert("Logged in");
+                                router.push("/");
+                            } else {
+                                setAlert("Email or password is wrong");
+                            }
+                        }}
+                    >
+                        {({
+                            errors,
+                            touched,
+                            handleSubmit,
+                            handleChange,
+                            handleBlur,
+                            isSubmitting,
+                        }) => (
+                            <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+                                <input id="email" name="email" type="email" onChange={handleChange} onBlur={handleBlur} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="E-mail"></input>
+                                { errors.email && touched.email ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{errors.email}</div> : null }
+                                <input id="password" name="password" type="password" onChange={handleChange} onBlur={handleBlur} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="Password"></input>
+                                { errors.password && touched.password ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{errors.password}</div> : null }
+                                { alert && <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded w-1/2">{alert}</div> }
+                                <button type="submit" className="flex border mt-8 rounded px-8 py-2 text-sm mx-auto border-indigo-500 text-indigo-500" disabled={isSubmitting}>Login</button>
+                            </form>
+                        )}
+                    </Formik>
                     <div className="text-gray-500 text-center my-6">
                         Don't have an account? <div className="inline text-indigo-500 cursor-pointer" onClick={() => router.push("/register")}>Register.</div>
                     </div>
--- pages/register.js
+++ pages/register.js
@@ -1,54 +1,18 @@
 import { useRouter } from "next/router";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import * as Yup from "yup";
 import { useState } from "react";
-import { signIn } from "next-auth/react";
 
 export default function Register() {
-    const [serverError, setServerError] = useState("");
+    const [alert, setAlert] = useState("");
     const router = useRouter();
 
-    const formik = useFormik({
-        initialValues: {
-            email : "",
-            password: "",
-        },
-        validationSchema: Yup.object({
-            email: Yup.string()
-                .email('Invalid email address.').required('You have to provide email address.'),
-            password: Yup.string()
-                .required('Please enter password.')
-                .min(8, 'Password has to be 8 characters minimum.'),
-        }),
-        onSubmit: async (values) => {
-            const response = await fetch("http://localhost:3000/api/auth/register", {
-                method: "POST",
-                headers: {
-                   "Content-type": "application/json"
-                },
-                body: JSON.stringify(values)
-            }).then((res) => {
-                return res.json();
-            }).catch((err) => {
-                return err.json();
-            });
-
-            if (response.status == "ok") {
-                const res = await signIn("credentials", {
-                    redirect: false,
-                    email: values.email,
-                    password: values.password,
-                    callbackUrl: "/",
-                });
-    
-                if (res.ok) {
-                    router.push("/");
-                }
-            } else {
-                setServerError(response.msg);
-            }
-        },
-    });
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .email('Invalid email address.').required('You have to provide email address.'),
+        password: Yup.string()
+            .required('Please enter password.')
+    })
 
     return (
         <div>
@@ -58,14 +22,45 @@ export default function Register() {
                     <div className="my-6 text-gray-600 text-center text-2xl">
                         Register
                     </div>
-                    <form className="flex flex-col items-center" onSubmit={formik.handleSubmit}>
-                        <input id="email" name="email" type="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="E-mail"></input>
-                        { formik.errors.email && formik.touched.email ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{formik.errors.email}</div> : null }
-                        <input id="password" name="password" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="Password"></input>
-                        { formik.errors.password && formik.touched.password ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{formik.errors.password}</div> : null }
-                        { serverError && <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded w-1/2">{serverError}</div> }
-                        <button type="submit" className="flex border mt-8 rounded px-8 py-2 text-sm mx-auto border-indigo-500 text-indigo-500">Register</button>
-                    </form>
+                    <Formik
+                        initialValues={{ email: "", password: "" }}
+                        validationSchema={validationSchema}
+                        onSubmit={async (values) => {
+                            let res = await fetch("http://localhost:3000/api/auth/register", {
+                                method: "POST",
+                                headers: {
+                                    "Content-type": "application/json"
+                                },
+                                body: JSON.stringify(values)
+                            });
+                            
+                            if (res.ok) {
+                                res = await res.json();
+                                setAlert(res.msg);
+                            } else {
+                                res = await res.json();
+                                setAlert(res.msg);
+                            }
+                        }}
+                    >
+                        {({
+                            errors,
+                            touched,
+                            handleSubmit,
+                            handleChange,
+                            handleBlur,
+                            isSubmitting,
+                        }) => (
+                            <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+                                <input id="email" name="email" type="email" onChange={handleChange} onBlur={handleBlur} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="E-mail"></input>
+                                { errors.email && touched.email ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{errors.email}</div> : null }
+                                <input id="password" name="password" type="password" onChange={handleChange} onBlur={handleBlur} className="block p-2 w-1/2 mt-8 bg-white border border-gray-400 rounded" placeholder="Password"></input>
+                                { errors.password && touched.password ? <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded">{errors.password}</div> : null }
+                                { alert && <div className="p-1 mt-1 text-sm text-red-700 bg-red-100 rounded w-1/2">{alert}</div> }
+                                <button type="submit" className="flex border mt-8 rounded px-8 py-2 text-sm mx-auto border-indigo-500 text-indigo-500" disabled={isSubmitting}>Register</button>
+                            </form>
+                        )}
+                    </Formik>
                     <div className="text-gray-500 text-center my-6">
                         Already have an account? <div className="inline text-indigo-500 cursor-pointer" onClick={() => router.push("/login")}>Login.</div>
                     </div>
