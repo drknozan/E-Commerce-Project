35ff5845d4737f12fb05290c3b7eca0f30d93c06
drknozan <drknozan@gmail.com>
2023-02-23 10:19:02 +0300

    feat: loading progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,34 @@ import Navbar from '../components/Navbar';
 import Sidebar from "../components/Sidebar";
 import { SessionProvider } from "next-auth/react";
 import Footer from '../components/Footer';
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    }
+
+    const handleStop = () => {
+      NProgress.done();
+    }
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    }
+  }, [router]);
+
   return (
     <>
       <SessionProvider session={pageProps.session}>
